2411fab8bc86e6040a9ad346071f84369bcda71e
Andre Bon Cipriano <andreboncipriano@gmail.com>
2024-10-14 15:25:12 +0800

    Updates
    - Hero Section Animation

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,15 +1,44 @@
+"use client";
+
+import { useEffect, useRef } from 'react';
 import Image from 'next/image';
 
-export default function Hero({ domain }) {
+export default function Hero({ domain, phrase = "Rise Above, Conquer Beyond!" }) {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    if (!phrase) return;
+
+    let index = 0;
+    const textElement = textRef.current;
+    textElement.innerHTML = ''; // Clear the text before typing starts
+
+    const type = () => {
+      if (index < phrase.length) {
+        textElement.innerHTML += phrase.charAt(index);
+        index++;
+        setTimeout(type, 100);
+      }
+    };
+
+    type(); // Start the typing effect
+
+    return () => {
+      index = phrase.length; // Cleanup to prevent overlap if the component re-renders
+    };
+  }, [phrase]);
+
   return (
     <section className="hero-section">
       <div className="overlay"></div>
       <div className="container container-padding">
         <div className="row align-items-center gy-6 gy-xl-0">
-          <div className="col-md-12">
-            <h1 className="mb-4 text">Rise Above, Conquer Beyond!</h1>
+          <div className="col-md-7">
+            <h1 className="display-2 typer">
+              <span ref={textRef}></span>
+            </h1>
           </div>
-          <div className="col-md-5 text-center d-none">
+          <div className="col-md-5 text-center d-none1">
             <div className="shape-wrapper">
               <div className="background-shape"></div>
               <Image 
