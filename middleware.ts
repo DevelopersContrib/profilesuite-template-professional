3577b1e06dd0463693756e8f55524d972dda4338
Ronan <row_none@yahoo.com>
2024-10-16 18:48:41 -0700

    testing.

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-// middleware.ts
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// // middleware.ts
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
-export function middleware(req: NextRequest) {
-    const host = req.headers.get('host')
-    if (!host?.includes('localhost')) {
-        return NextResponse.redirect(`https://${host}${req.nextUrl.pathname}`, 301);
-    }
-}
+// // This function can be marked `async` if using `await` inside
+// export function middleware(req: NextRequest) {
+//     const host = req.headers.get('host')
+//     if (!host?.includes('localhost')) {
+//         return NextResponse.redirect(`https://${host}${req.nextUrl.pathname}`, 301);
+//     }
+// }
